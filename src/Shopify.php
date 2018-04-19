5f5590a1da06bddc955aea964a5aa1f3e49224bb
baberuth22 <bryan@sltwtr.com>
2018-04-19 11:06:48 -0700

    fixed api call problem by using private app method

--- src/Shopify.php
+++ src/Shopify.php
@@ -89,15 +89,24 @@ class Shopify
      */
     private function request(string $method, string $uri, array $payload): array
     {
-        $response = $this->client->request(
-            $method, 
-            "https://{$this->shop}{$uri}",
-            [
+        $url = "https://{$this->shop}{$uri}";
+        // $url = "https://{$this->key}:{$this->token}@{$this->shop}{$uri}";
+        $params = [
                 'exceptions' => false,
                 'json' => $payload,
                 'headers' => $this->token ? ['X-Shopify-Access-Token' =>$this->token, 'Content-Type' => 'application/json'] : []
-            ]
+        ];
+
+        $response = $this->client->request(
+            $method, 
+            $url,
+            $params
         );
+
+        if( $uri !== '/admin/oauth/access_token' ) {
+          $shopifyResponse = $this->shopify($uri, $method, $payload);
+          return $shopifyResponse;
+        }
 
         return array_merge([
             'statusCode' => $response->getStatusCode(),
@@ -105,4 +114,115 @@ class Shopify
             'callLimit' => $response->hasHeader('HTTP_X_SHOPIFY_SHOP_API_CALL_LIMIT') ? $response->getHeaders()['HTTP_X_SHOPIFY_SHOP_API_CALL_LIMIT'][0] : '',
         ],json_decode($response->getBody(),true));
     }
+
+    // THESE FUNCTIONS ARE FROM ATB
+    private function shopify($api_call,$method=null,$data=null){
+     
+      $shopify = $this->call_shopify($api_call,$method,$data);
+     
+      if ( !empty($shopify->errors) ) {
+        sleep(1);
+        $shopify = $this->call_shopify($api_call,$method,$data);
+
+      }
+
+      $debug = array('call'=>$api_call,'method'=>$method,'data'=>$data,'response'=>$shopify);
+
+      return $shopify;
+    }
+
+
+    private function call_shopify($api_call,$method='GET',$data=null){
+
+      global $model;
+
+      $child_parent = array(
+        'articles' => 'blogs',
+      );
+
+      if( isset( $child_parent[$api_call] ) ){
+        if( isset( $data['parent_id'] ) ){
+          $api_call = $child_parent[$api_call] . '/' . $data['parent_id'] . '/' . $api_call;
+        }
+      }
+      
+      // $url = 'https://'.$model->shopify->api_key.':'.$model->shopify->api_password.'@'.$model->shopify->shop_id.'.myshopify.com/admin/'.$api_call.'.json';
+      $url = 'https://'.$this->key.':'.$this->token.'@'.$this->shop.$api_call;
+
+      if($method == 'GET' && count($data))
+        $url.= '?' . http_build_query($data);
+
+      $session = curl_init( $url );
+
+      //curl_setopt($session, CURLOPT_URL, $url);
+      curl_setopt($session, CURLOPT_HEADER, false);
+      curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+      if($method) {
+        curl_setopt($session, CURLOPT_CUSTOMREQUEST, $method);
+      }
+      if($data) {
+        $data_json = json_encode( $data );
+
+        if($method == 'GET' && count($data)){
+          curl_setopt($session, CURLOPT_HTTPGET, 1); 
+        } else if($method == 'POST'){
+          curl_setopt($session, CURLOPT_HTTPHEADER, array(                                                                          
+          'Content-Type: application/json',                                                                               
+          ));
+          curl_setopt($session, CURLOPT_POSTFIELDS, $data_json); 
+        }else if( $method == 'PUT' ){
+
+          curl_setopt($session, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: POST') );
+          curl_setopt($session, CURLOPT_POSTFIELDS, $data_json);
+          curl_setopt($session, CURLOPT_CUSTOMREQUEST, 'PUT');
+          curl_setopt($session, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        }
+      }
+
+      // if(ereg("^(https)",$url)) curl_setopt($session,CURLOPT_SSL_VERIFYPEER,false);
+      if(preg_match('/^(https)/',$url)) {
+        curl_setopt($session,CURLOPT_SSL_VERIFYPEER,false);
+      } 
+      // this function is called by curl for each header received
+      $headers = [];
+      curl_setopt($session, CURLOPT_HEADERFUNCTION,
+        function($curl, $header) use (&$headers)
+        {
+          $len = strlen($header);
+          $header = explode(':', $header, 2);
+          if (count($header) < 2) // ignore invalid headers
+            return $len;
+
+          $name = strtolower(trim($header[0]));
+          if (!array_key_exists($name, $headers))
+            $headers[$name] = [trim($header[1])];
+          else
+            $headers[$name][] = trim($header[1]);
+
+          return $len;
+        }
+      );
+
+      $response = curl_exec($session);
+      $httpcode = curl_getinfo($session, CURLINFO_HTTP_CODE);
+      $call_limit = ( isset($headers['http_x_shopify_shop_api_call_limit']) && isset($headers['http_x_shopify_shop_api_call_limit'][0]) ) ? $headers['http_x_shopify_shop_api_call_limit'][0] : '';
+      // Check for errors and display the error message
+      $error_message = '';
+      if($errno = curl_errno($session)) {
+          $error_message = curl_strerror($errno);
+          echo "{$errno}:\n {$error_message}";
+      }
+
+      $response = json_decode($response);
+      curl_close($session);
+
+      return array_merge([
+            'statusCode' => $httpcode,
+            'reasonPhrase' => $error_message,
+            'callLimit' => $call_limit,
+        ],json_decode(json_encode($response),true));
+
+    }
+
 }
+
